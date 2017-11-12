b157ce3867d1cd9dfb9724c19afd1c1f544bd311
Eric Davis <Eric.Davis2@stjude.org>
2017-11-12 15:33:17 -0600

    flailing

--- lookup_variant.py
+++ lookup_variant.py
@@ -1,9 +1,11 @@
 import sqlite3 as sql
 import os
 from flask.ext.cache import Cache
-from flask import Flask, render_template, json, request, redirect, url_for
+from flask import Flask, render_template, json, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'You Will Never Guess'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 cache = Cache(app, config={'CACHE_TYPE': 'redis'})
 
@@ -11,21 +13,19 @@ cache = Cache(app, config={'CACHE_TYPE': 'redis'})
 def main():
     return render_template('home.html')
 
-
-@app.route('/result', methods=["POST", "GET"])
-def result():
-    return "result.html"
-
 @app.route('/search', methods=["POST", "GET"])
 def search():
     data = json.dumps(request.json)
-    #TODO parse data
-    
+    return redirect(url_for("whatever", chrom='1', start='35146'))
+
+@app.route('/whatever/<chrom>/<start>', methods=["POST", "GET"])
+def whatever(chrom, start):
+
+    query = "SELECT * FROM variants WHERE chrom={} and hg19_pos={}".format(chrom, start)
     conn = sql.connect("REVEL.db")
     conn.row_factory = sql.Row
     cur = conn.cursor()
-    rows = cur.execute("SELECT * FROM variants WHERE chrom={} and hg19_pos={}".format('1', '35146'))
-
+    rows = cur.execute(query)
     return render_template("result.html", rows=rows)
 
 if __name__ == "__main__":

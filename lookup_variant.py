0a55074e3e5b76527ec12fcdb3993c3312265fb3
Eric Davis <Eric.Davis2@stjude.org>
2017-11-12 16:54:58 -0600

    added db query

--- lookup_variant.py
+++ lookup_variant.py
@@ -21,6 +21,12 @@ def search():
     if db.lower() != 'revel':
         return "Sorry, only the revel database is supported", 404
 
+    if not chrom:
+        return "Must provide a valid Chromosome", 404
+
+    if not coord:
+        return "Must provide a valid Coordinate", 404
+
     return redirect(url_for("result", chrom=chrom, start=coord))
 
 
@@ -35,7 +41,10 @@ def result(chrom, start):
              "aaref": "TT",
              "aaalt": "TT",
              "REVEL": 0.01}]
-
+    conn = sql.connect("REVEL.db")
+    conn.row_factory = sql.Row
+    cur = conn.cursor()
+    rows = cur.execute("SELECT * FROM variants WHERE chrom={} and hg19_pos={}".format(chrom, start))
     return render_template("result.html", rows=rows)
 
 if __name__ == "__main__":

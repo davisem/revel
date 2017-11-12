9dff001fd520101638d470af5f61b2b5015552ab
Eric Davis <Eric.Davis2@stjude.org>
2017-11-12 13:49:09 -0600

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,6 @@
-FROM ubuntu:16.04
+FROM python:3.4-alpine
 
-RUN apt-get update -y && apt-get install
-RUN apt-get install -y python-dev python-pip
-
-ADD . /app/
-ENV HOME /app/
-WORKDIR /app/
-
-RUN pip install uwsgi
+ADD . /code
+WORKDIR /code
 RUN pip install -r ./requirements.txt
-
-EXPOSE 8000
-
-ENV FLASK_APP /app/app.py
-
-ENTRYPOINT ["uwsgi", "--http", "0.0.0.0:8000", "--module", "app:__main__", "--processes", "1", "--threads", "8"]
-
-
-pip freeze > requirements.txt
-docker up
-docker run -d
+CMD ["python", "lookup_variant.py"]
--- lookup_variant.py
+++ lookup_variant.py
@@ -1,26 +1,32 @@
 import sqlite3 as sql
+import os
+from flask.ext.cache import Cache
 from flask import Flask, render_template, json, request, redirect, url_for
 
 app = Flask(__name__)
 
+cache = Cache(app, config={'CACHE_TYPE': 'redis'})
+
 @app.route('/')
 def main():
     return render_template('home.html')
-@app.route('/result', methods=["GET"])
+
+
+@app.route('/result', methods=["POST", "GET"])
 def result():
-    conn = sql.connect("REVELmock.db")
+    return "result.html"
+
+@app.route('/search', methods=["POST", "GET"])
+def search():
+    data = json.dumps(request.json)
+    #TODO parse data
+    
+    conn = sql.connect("REVEL.db")
     conn.row_factory = sql.Row
     cur = conn.cursor()
-    cur.execute("SELECT * FROM variants WHERE chrom={} and pos={}".format('1', '100'))
-    rows = [{"chrom":"1", "pos":"100", "ref":"A", "alt":"T","REVEL":".89"}]
+    rows = cur.execute("SELECT * FROM variants WHERE chrom={} and hg19_pos={}".format('1', '35146'))
+
     return render_template("result.html", rows=rows)
 
-@app.route('/search', methods=["POST"])
-def search():
-    
-    data = json.dumps(request.json)
-
-    return redirect(url_for('result'))
-
 if __name__ == "__main__":
-    app.run(port=5002)
+    app.run(host="0.0.0.0", port=5000, debug=True)
